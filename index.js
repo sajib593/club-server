06e9e038c0184f15932545b7724f274bafb69445
sajib593 <sajib.hoossain11@gmail.com>
2025-12-06 20:00:15 -0800

    mongodb install

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express()
 const port = process.env.PORT || 3000
 
 
-
+// llrGAtLrl8ktqNFj 
 // middleware
 app.use(express.json());
 app.use(cors());
@@ -13,6 +13,39 @@ app.use(cors());
 app.get('/', (req, res) => {
   res.send('Hello club!')
 })
+
+
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://club:llrGAtLrl8ktqNFj@cluster0.vkkq9zu.mongodb.net/?appName=Cluster0";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
